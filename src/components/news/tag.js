e540c7c81d0cc40ec76470441d13dd1def001d23
Matt Watson <mattwat@gmail.com>
2021-03-01 19:05:32 -0500

    use custom link component for tags

--- src/components/news/tag.js
+++ src/components/news/tag.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
 import styled from 'styled-components'
+import { Link } from '../link'
 
 export const BaseTag = styled.span(({ theme }) => `
   margin: 0;
